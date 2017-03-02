6a8b95610c5ba86595c8f9ef3527bd9139759467
Jakub Gac <jakubgac@Mac-Pro-Jakub.local>
2017-03-02 11:44:53 +0100

    Initial Commit

---
+++ walidator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  walidator
+//
+//  Created by Jakub Gac on 02.03.2017.
+//  Copyright © 2017 Jakub Gac. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
